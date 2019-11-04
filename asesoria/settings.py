f6eb5b5f8d8d8aab4e99159d3f445a8a237038af
Thomas <thomasmoranqn@gmail.com>
2019-11-04 16:33:47 -0300

    Primer commit

--- asesoria/settings.py
+++ asesoria/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'crispy_forms',
+    'phonenumber_field',
+]
+
+LOCAL_APPS = [
+    'apps.asesoria',
+]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
